e8586df8fbbba92dbf226d88b93bc0d1a168593d
ProkopenkoAleksandr <105602656+ProkopenkoAleksandr@users.noreply.github.com>
2026-05-29 16:47:17 +0300

    Add files via upload

---
+++ HiTechClassic-Shop/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBsc5Mz9eooW1wgp2JXrNvIfaHFewXNAzA",
+  authDomain: "me-shop-bf0a7.firebaseapp.com",
+  databaseURL: "https://me-shop-bf0a7-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "me-shop-bf0a7",
+  storageBucket: "me-shop-bf0a7.firebasestorage.app",
+  messagingSenderId: "816771532922",
+  appId: "1:816771532922:web:401b081c4623f0f3b153c3"
+};
